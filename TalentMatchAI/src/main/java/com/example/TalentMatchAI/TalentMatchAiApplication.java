374e332bdf2849fdee3b2b0f17bb55885fb80229
boj4ckk <yazkilito@gmail.com>
2026-03-06 12:55:14 +0100

    Init app

---
+++ TalentMatchAI/src/main/java/com/example/TalentMatchAI/TalentMatchAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TalentMatchAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentMatchAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentMatchAiApplication.class, args);
+	}
+
+}
---
+++ TalentMatchAI/src/test/java/com/example/TalentMatchAI/TalentMatchAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TalentMatchAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentMatchAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
